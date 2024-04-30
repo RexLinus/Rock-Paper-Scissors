4704caff848fecfc499eb55f5f4b8925edc3345b
Reza <reza.linusx@gmail.com>
2024-04-30 13:07:14 +0330

    Respond to user when clicking on options

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ const options = document.querySelectorAll(".gameOption");
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
+
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    game(option.id);
+  });
+});
+
+function game(id) {
+  console.log(id);
+}
